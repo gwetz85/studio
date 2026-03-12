cc11f197a478d57299c794a94f86853d60028f48
Agus Suriyadi <agus.sahabathanura@gmail.com>
2026-03-12 20:40:23 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-9800760424-be9df",
+  "appId": "1:659514293713:web:e0a7ea4780af9764eef4f0",
+  "apiKey": "AIzaSyD--yCeOjW9FvVmqYfJz6Z6XKuY2mRe3tU",
+  "authDomain": "studio-9800760424-be9df.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "659514293713"
+};
